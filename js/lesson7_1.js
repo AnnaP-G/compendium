f112c2be148c208cd6ec2c91f96ef244ba37fc3a
AnnaP-G <klientkv1511@gmail.com>
2024-01-03 19:22:08 +0200

    comit0201

--- js/lesson7_1.js
+++ js/lesson7_1.js
@@ -3,6 +3,15 @@
 // *** 3. Document
 // *** 4. Типи елементів в DOM дереві
 // *** 5. Пошук елементів в DOM дереві
+//        * querySelector
+//        * querySelectorAll
+//        * getElementById
+//        * getElememtsByNyme
+//        * getElementsByClassName
+//        * getElementsByTagName
+// *** 6. 
+
+
 
 // *** 1. BOM - Browser Object Model - window
 
@@ -68,8 +77,10 @@
 
 
 
+
+
 // *** 5. Пошук елементів в DOM дереві
-// ** З пошуку поичнаємо писати код
+// ** З пошуку починаємо писати код
 // ** Шукаємо в document
 // ** Якщо шукаємо неіснуючий елемент, то запишеться null
 
@@ -80,12 +91,46 @@
 // const elem = document.querySelector('.js-test')
 // console.log(elem);
 
-// *  const elem = document.querySelector('#categories')
+// const elem = document.querySelector('#categories')
 // console.log(elem);
-// const liElem = elem.querySelector('li'); // пошук за елементом, вже не через document
+// const liElem = elem.querySelector('.item:nth-child(2)'); // пошук за класом, другу лішку, вже не через document
 // console.log(liElem);
 
 // *** querySelectorAll - поверне псевдомасив
-const elem = document.querySelector('#categories')
-const liElemes = elem.querySelectorAll('.item')
-console.log(liElemes);
+// const elem = document.querySelector('#categories')
+// const liElemes = elem.querySelectorAll('.item')
+// console.log(liElemes);
+
+// ** як отримати другу li
+// *пошук за класом
+// const elem = document.querySelector('#categories')
+// const liElemes = elem.querySelectorAll('.item:nth-child(2)')
+// console.log(liElemes);
+
+// * пошук за тегом в псевдомасиві
+// const elem = document.querySelector('#categories')
+// const liElemes = elem.querySelectorAll('.item')
+// console.log(liElemes[1]);
+
+// * Перетворюємо псевдомасив на масив
+// const elem = document.querySelector('#categories')
+// const liElemes = elem.querySelectorAll('.item')
+// console.log(Array.from(liElemes)); || console.log(...liElemes);
+
+// *** getElementById
+// const idElem = document.getElementById('categories'); // решітка в цьому випадку не пишеться
+// console.log(idElem);
+
+// *** getElememtsByNyme
+// const idElem = document.getElementsByName('viewport');
+// console.log(idElem);
+
+// *** getElementsByClassName
+// const idElem = document.getElementsByClassName('js-test');
+// console.log(idElem);
+
+// *** getElementsByTagName
+// const idElem = document.getElementsByTagName('button');
+// console.log(idElem);
+
+
